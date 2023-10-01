0186c9818ae475c542637bb16c2ddd7595123674
yisus1002 <yisusgarcia1002@gmail.com>
2023-10-01 15:03:23 -0500

    Navbar toggle added

--- src/components/shared/Nabvar/NavBar.vue
+++ src/components/shared/Nabvar/NavBar.vue
@@ -1,7 +1,9 @@
-<template lang="">
-      <ul class="space-y-2 font-medium">
+<template >
+      <ul class="space-y-2 font-medium color">
          <li v-for="(menu, index) in items" :key="index">
-            <router-link :to="menu.to" class="aside__item group" :class="{ ' activated': $route.path === menu.to}" aria-current="page" >
+            <router-link
+            @click="toogleH"
+                :to="menu.to" class="aside__item group" :class="{ ' activated': $route.path === menu.to}" aria-current="page" >
               <i :class=" menu.icon"></i>
                <span class="ml-3">{{menu.title}}</span>
             </router-link>
@@ -17,6 +19,11 @@ export default {
                   { title: 'Home', icon: 'fa-solid fa-house', to:'/Home' },
                   { title: 'Hotel', icon: 'fa-solid fa-hotel', to:'/hotel' },
                   ],
+    }
+  },
+  methods:{
+    toogleH(){
+      this.$emit('clase-toggled');
     }
   }
   
