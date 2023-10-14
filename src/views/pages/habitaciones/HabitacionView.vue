7661d0315bbf8de3f680ffa6b4935c5f6b24b126
Jesus Garcia <yisusgarcia1002@gmail.com>
2023-10-14 14:20:46 -0500

    yarn was removed and styles were added to rooms

--- src/views/pages/habitaciones/HabitacionView.vue
+++ src/views/pages/habitaciones/HabitacionView.vue
@@ -11,12 +11,11 @@ export default {
 </script>
 <style scoped>
 .fondo{
-    height: 100vh;
+    background-image: url('https://img.freepik.com/fotos-premium/interior-dormitorio-futurista-holografico-inteligente-moderno-alta-tecnologia-ciencia-ficcion-cyberpunk-decoracion-hogar-3d_839035-2515.jpg?w=826');
+    min-height: 100vh;
+        height: 100%;
         width: 100%;
-        /* object-fit: cover; */
-        background-image: url('https://img.freepik.com/fotos-premium/interior-dormitorio-futurista-holografico-inteligente-moderno-alta-tecnologia-ciencia-ficcion-cyberpunk-decoracion-hogar-3d_839035-2515.jpg?w=826');
         background-repeat: no-repeat;
-        /* background-size:100% ; */
         background-attachment: fixed;
         background-size:cover ;
 }    
--- src/views/pages/hoteles/HotelesView.vue
+++ src/views/pages/hoteles/HotelesView.vue
@@ -13,10 +13,10 @@ export default {
 </script>
 <style scoped>
     .fondo{
+        background-image: url('https://img.freepik.com/foto-gratis/casa-lago-casa-iluminada-iluminada-noche_1340-43162.jpg?w=740&t=st=1696660596~exp=1696661196~hmac=dae80e9c1c25e19a5c625b6ff1f03c00f5ca629814d3c3d38c45c51e7c012450');
         min-height: 100vh;
         height: 100%;
         width: 100%;
-        background-image: url('https://img.freepik.com/foto-gratis/casa-lago-casa-iluminada-iluminada-noche_1340-43162.jpg?w=740&t=st=1696660596~exp=1696661196~hmac=dae80e9c1c25e19a5c625b6ff1f03c00f5ca629814d3c3d38c45c51e7c012450');
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-size:cover ;
