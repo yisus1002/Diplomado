c808b565690b40e514fe372a34d6160a32bea627
Jesus Garcia <yisusgarcia1002@gmail.com>
2023-10-14 15:11:38 -0500

    improved global styles

--- src/views/pages/hoteles/pages/hotel-info/hotelInfor.vue
+++ src/views/pages/hoteles/pages/hotel-info/hotelInfor.vue
@@ -1,28 +1,21 @@
 <template >
-    <div class="Hotel_home" >
+    <div class="Hotel_home Hotel__home" >
             <h1 class="Hotel__home__title">Información Hotel {{ id }}</h1>
         <hr class="mb-6 bg-gray-400 h-1" />
         <button type="button" class="button_back" @click="goBack"><i class="fa-solid fa-arrow-left"></i> Regresar</button>
         
         <div class="container_detalle"> 
             <div class="flex items-center justify-center p-3">
-                    <img src="../../../../../assets/images/hotel/habitaciones.jpg" class="rounded-xl max-w-60 max-h-60 mx-auto">
+                    <img src="https://loremflickr.com/cache/resized/65535_52546335077_3169299263_c_640_480_nofilter.jpg" class="rounded-xl max-w-60  mx-auto">
                 </div>
             <div class="container_form">
-                <form  ref="Detalle_hotel"  @submit.prevent="send"></form>
                 <div class="grid gap-6 mb-6 md:grid-cols-1">
-                    <div>
-                        <h1 class="detaller_form">Detalle del hotel: {{ Detalle_hotel.hotel }}</h1>
-                    </div>
+                        <h1 class="detaller_form">{{ Detalle_hotel.hotel }}</h1>
                     <div class="grip  gap-6 mb-6 md:grid-cols-2">
-                        <div>
-                            <h2 class="detalle_h2">Nit: {{ Detalle_hotel.nit }}</h2>
-                        </div>
-                        <div>
-                            <h2 class="detalle_h2">Ciudad: {{ Detalle_hotel.ciudad }}</h2>
-                        </div>
-                        <h2 class="detalle_h2">Direccion: {{ Detalle_hotel.direccion }}</h2>
-                        <h2 class="detalle_h2">Habitaciones: {{ Detalle_hotel.habitaciontotal }}</h2>
+                            <P class="detalle_h2">Nit: {{ Detalle_hotel.nit }}</P>
+                            <P class="detalle_h2">Ciudad: {{ Detalle_hotel.ciudad }}</P>
+                            <P class="detalle_h2">Direccion: {{ Detalle_hotel.direccion }}</P>
+                            <P class="detalle_h2">Habitaciones: {{ Detalle_hotel.habitaciontotal }}</P>
                     </div>
                 </div>
                 
